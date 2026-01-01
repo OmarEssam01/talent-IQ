568ae7726e8dc753880337d12418bfefc80ee90f
OmarEssam01 <omaressam01@gmail.com>
2026-01-02 00:19:26 +0200

    db and tools setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+import {ENV} from "./env.js"
+export const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(ENV.DB_URL)
+      console.log("Connected to Mongo DB : " ,  conn.connection.host);
+    } catch (error) {
+        console.error(" X Error Connecting to Mongo DB" , error)
+        process.exit(1)
+    }
+}
